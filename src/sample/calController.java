49d675f5ce4314e69de606cfda720d410540aa7d
MjMb23 <64239666+MjMb23@users.noreply.github.com>
2020-11-02 17:37:00 +0800

    buttons id and controller

---
+++ src/sample/calController.java
@@ -0,0 +1,284 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class calController {
+
+    @FXML
+    private TextField valuesField;
+
+    @FXML
+    private TextField inputField;
+
+    @FXML
+    private TextField partialAnswerField;
+
+    @FXML
+    private Button delButton;
+
+    @FXML
+    private Button acButton;
+
+    @FXML
+    private Button negateButton;
+
+    @FXML
+    private Button _7Button;
+
+    @FXML
+    private Button _8Button;
+
+    @FXML
+    private Button _9Button;
+
+    @FXML
+    private Button multiplyButton;
+
+    @FXML
+    private Button divideButton;
+
+    @FXML
+    private Button _4Button;
+
+    @FXML
+    private Button _5Button;
+
+    @FXML
+    private Button _6Button;
+
+    @FXML
+    private Button subtractButton;
+
+    @FXML
+    private Button _1Button;
+
+    @FXML
+    private Button _2Button;
+
+    @FXML
+    private Button _3Button;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button _0Button;
+
+    @FXML
+    private Button pointButton;
+
+    @FXML
+    private Button yRootButton;
+
+    @FXML
+    private Button squareRootButton;
+
+    @FXML
+    private Button nestedPowButton;
+
+    @FXML
+    private Button powoOfYButton;
+
+    @FXML
+    private Button powOf2Button;
+
+    @FXML
+    private Button moduloButton;
+
+    @FXML
+    private Button factorialButton;
+
+    @FXML
+    private Button logOfXButton;
+
+    @FXML
+    private Button logXofYButton;
+
+    @FXML
+    private Button summationButton;
+
+    @FXML
+    private Button ansButton;
+
+    @FXML
+    private Button floorButton;
+
+    @FXML
+    private Button ceilButton;
+
+    @FXML
+    private Button nestedSummationButton;
+
+    @FXML
+    private Button equalsButton;
+
+    @FXML
+    void _0ButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _1ButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _2ButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _3ButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _4ButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _5ButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _6ButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _7ButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _8ButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _9ButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void acButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ansButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ceilButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void divideButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void equalsButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void factorialButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void floorButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOfXButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logXofYButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void moduloButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void multiplyButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void negateButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nestedPowButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nestedSummationButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pointButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void powOf2ButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void powOfYButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void squareRootButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void subtractButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void summationButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void yRootButtonClick(ActionEvent event) {
+
+    }
+
+
+}
