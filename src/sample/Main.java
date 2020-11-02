2612113a9d39f70727e547055e62fa3741093572
MjMb23 <64239666+MjMb23@users.noreply.github.com>
2020-11-02 16:22:44 +0800

    Draft UI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("AdvanceCalc_UI.fxml"));
         primaryStage.setTitle("AdvanCalc");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
