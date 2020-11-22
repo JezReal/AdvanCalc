b0944e648b69fb6d0c7ba7f5b7b126dabafd649e
go-roii <royfgopez80@gmail.com>
2020-11-22 11:38:51 +0800

    revert to resizable window

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,12 @@
 package app;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,8 +17,10 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("AdvanceCalc_UI.fxml"));
         primaryStage.setTitle("AdvanCalc");
         primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
+        primaryStage.sizeToScene();
         primaryStage.show();
+        primaryStage.setMinWidth(primaryStage.getWidth());
+        primaryStage.setMinHeight(primaryStage.getHeight());
     }
 
 
